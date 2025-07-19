8e3a2b196c88175cdd8815eca76d50d3771cdd3f
Pedro F. Albanese <68971450+pedroalbanese@users.noreply.github.com>
2025-07-19 12:32:45 -0300

    Update main.go

--- cmd/xxe/main.go
+++ cmd/xxe/main.go
@@ -6,13 +6,12 @@ import (
 	"io"
 	"log"
 	"os"
-
+	
 	"github.com/pedroalbanese/xxencode"
 )
 
 var (
-	dec   = flag.Bool("d", false, "Decode instead of Encode")
-	ifile = flag.String("f", "", "Target file")
+	dec = flag.Bool("d", false, "Decode instead of Encode")
 )
 
 func main() {
@@ -25,66 +24,67 @@ func main() {
 	}
 
 	if !*dec {
-		// Codificação
-		infile, err := os.Open(*ifile)
-		if err != nil {
-			log.Fatal(err)
-		}
-		defer infile.Close()
+		// Modo codificação
+		mw := xxencode.NewMultiWriter(os.Stdout)
+		defer mw.Close()
 
-		info, err := infile.Stat()
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		xxw := xxencode.NewWriter(os.Stdout, *ifile, info.Mode())
-		if _, err := io.Copy(xxw, infile); err != nil {
-			log.Fatal(err)
-		}
-		if err := xxw.Flush(); err != nil {
-			log.Fatal(err)
-		}
-	} else {
-		// Decodificação
-		var infile *os.File
-		var err error
-		
-		if *ifile == "-" || *ifile == "" {
-			infile = os.Stdin
-		} else {
-			infile, err = os.Open(*ifile)
+		for _, filename := range flag.Args() {
+			file, err := os.Open(filename)
 			if err != nil {
 				log.Fatal(err)
 			}
-			defer infile.Close()
+
+			info, err := file.Stat()
+			if err != nil {
+				file.Close()
+				log.Fatal(err)
+			}
+
+			if err := mw.WriteFile(filename, info.Mode(), file); err != nil {
+				file.Close()
+				log.Fatal(err)
+			}
+			file.Close()
 		}
+	} else {
+		// Modo decodificação - lê do primeiro argumento não-flag
+		if flag.NArg() == 0 {
+			log.Fatal("No input file specified for decoding")
+		}
+		inputFile := flag.Arg(0)
 
-		xxr := xxencode.NewReader(infile, nil)
-
-		filename, err := xxr.File()
+		file, err := os.Open(inputFile)
 		if err != nil {
 			log.Fatal(err)
 		}
+		defer file.Close()
 
-		outfile, err := os.Create(filename)
-		if err != nil {
-			log.Fatal(err)
+		mr := xxencode.NewMultiReader(file)
+
+		for {
+			fileInfo, reader, err := mr.Next()
+			if err == io.EOF {
+				break
+			}
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			outFile, err := os.Create(fileInfo.Name)
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			if _, err := io.Copy(outFile, reader); err != nil {
+				outFile.Close()
+				log.Fatal(err)
+			}
+
+			if err := outFile.Chmod(fileInfo.Mode); err != nil {
+				log.Printf("Warning: could not set mode for %s: %v", fileInfo.Name, err)
+			}
+			outFile.Close()
+			fmt.Printf("file: %s mode: %03o\n", fileInfo.Name, fileInfo.Mode)
 		}
-		defer outfile.Close()
-
-		if _, err := io.Copy(outfile, xxr); err != nil {
-			log.Fatal(err)
-		}
-
-		mode, err := xxr.Mode()
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		if err := outfile.Chmod(mode); err != nil {
-			log.Println("Warning: could not set file mode:", err)
-		}
-
-		fmt.Fprintf(os.Stderr, "file: %s, mode: %03o\n", filename, mode)
 	}
 }
